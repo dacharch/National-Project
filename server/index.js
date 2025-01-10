3d69af7f63bc4da3137c9c2da455f57441eac4e2
Neeraj Kumar <nk35715945@gmail.com>
2025-01-10 22:20:06 +0530

    Updation with whole project

--- server/index.js
+++ server/index.js
@@ -12,13 +12,17 @@ dotenv.config();
 connectDB();
 
 
-app.use(
-  cors({
-    origin: ["https://national-project-frontend.vercel.app/"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+
+  res.setHeader('Access-Control-Allow-Origin', 'https://national-project-frontend.vercel.app'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); 
+
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+
+  next();
+
+});
+
 app.use(cookieParser());
 app.use(express.json());  
 app.use("/", authRoute);
